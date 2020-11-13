d8d7a4d46d0b1d8c630c2d24b642756da2fabcb4
unknown <smolenski95@gmail.com>
2020-11-13 21:26:23 +0100

    test commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\michal\desktop\185ic\aplikacje webowe\aplikacje-internetowe-smolenski-185ic\aplikacje-internetowe-smolenski-185ic\lab1\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
