4dd7cc930f19117fef9f0bd761159c250151a3d4
unknown <smolenski95@gmail.com>
2020-11-13 22:18:27 +0100

    oby ostatni

--- Lab1/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\michal\desktop\185ic\aplikacje webowe\aplikacje-internetowe-smolenski-185ic\aplikacje-internetowe-smolenski-185ic\lab1\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
